fede7286d3be3db210bd7f55166d9eb6ba975c58
Allan <allanfelipedk@gmail.com>
2022-03-09 14:12:44 -0300

    Desafio - Reproduzindo o dialogo do pokemon / 2_Funcoes

---
+++ Introdução a Programação com C#/2_Funções/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio___Reproduzindo_o_dialogo_do_pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }     
+
+        static void MinhaFuncaoComParametros(string a, int x, int y)
+        {
+            //comandos da funcao
+        }
+        static void MinhaFuncaoSemParametros()
+        {
+            //comandos da funcao
+        }
+        static string FuncaoComRetorno()
+        {
+            return "Oi"; //-> linha obrigatoria
+        }
+        static string ReadNameInput(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
