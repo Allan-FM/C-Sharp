4d36ead117cf9e9d943e18710e6f99d9c1baf0d1
Allan <allanfelipedk@gmail.com>
2022-03-09 08:34:53 -0300

    1_Variables

---
+++ Introdução a Programação com C#/1_Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagemDoUsuario;
+            mensagemDoUsuario = Console.ReadLine();
+            Console.WriteLine(mensagemDoUsuario);
+
+            //  Tipo de Variaveis
+
+            int x = 2;
+            float y = 2.5f;
+            
+            long myLong = 5000;
+            double myDouble = 12345.12345d;
+
+            char myChar = 'A';
+            string myString = "Muitos char!";
+
+            bool myBool = true; //-> Só true ou False
+
+
+            Console.ReadKey();
+        }
+    }
+}
