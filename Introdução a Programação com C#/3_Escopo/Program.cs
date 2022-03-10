5001c4bc6ab2d208060bb0381938ef1d2e1d130a
Allan <allanfelipedk@gmail.com>
2022-03-10 01:31:02 -0300

    exercio - Pokemon modificado / 3_Escopo

---
+++ Introdução a Programação com C#/3_Escopo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_Escopo
+{
+    class Program
+    {
+        static int variavelDaClasse = 1;
+        static void Main(string[] args)
+        {
+            
+            Teste();
+            //Console.WriteLine(variavelDaFuncao); -> Esta fora do escopo
+            Console.WriteLine(variavelDaClasse);// -> Dentro do escopo "global"
+            Console.ReadKey();
+        }
+        static void Teste()
+        {
+            int variavelDaFuncao;
+            Console.WriteLine(variavelDaClasse);
+        }
+    }
+}
