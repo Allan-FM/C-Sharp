982a13a2575cb572f055e53bc5c650ffb99ae371
Allan <allanfelipedk@gmail.com>
2022-03-12 19:44:58 -0300

    Controle de Fluxo (if / else ) + Desafio Qual e o melhor jogo do universo ultilizando if/else

---
+++ Introdução a Programação com C#/5_Controle De Fluxo (if)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5_Controle_De_Fluxo__if_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha uma das opcoes -> Iniciante, Normal ou Expert.");
+            string difficultyLevel = Console.ReadLine();
+            //iniciante 1x enemyPower, normal 2x enemyPower e expert 5x enemyPower
+            int enemyPower = 20;    
+            if(difficultyLevel == "Iniciante")
+            {
+                Console.WriteLine("Voce escolheu a opcao iniciante");
+                Console.WriteLine($"Forca do inimigo -> {enemyPower}");
+            }
+            else if(difficultyLevel == "Normal")
+            {
+                Console.WriteLine("Voce escolheu a opcao iniciante");
+                Console.WriteLine($"Forca do inimigo -> {enemyPower * 2}");
+            }
+            else if(difficultyLevel == "Expert")
+            {
+                Console.WriteLine("Voce escolheu a opcao iniciante");
+                Console.WriteLine($"Forca do inimigo -> {enemyPower * 5}");
+            }
+            else
+            {
+                Console.WriteLine("Opcao nao encontrada! Escolha: Iniciante, Normal ou Expert");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introdução a Programação com C#/Exercicios/Desafio - Qual é o melhor jogo do mundo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Desafio___Qual_é_o_melhor_jogo_do_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintLineAndWait("Ola eu sou um genio e posso te conceder 3 desejos...");
+            PrintLineAndWait("Mas antes me responda uma pergunta. Voce tem 3 tentativas.");
+            string choiceGame = ReadTextInput("Qual e o melhor jogo do universo -> ");
+            string bestGame = "God Of War 4";
+            if(choiceGame != bestGame)
+            {
+                PrintLineAndWait("Resposta errada! Voce tem mais 2 tentativas.");
+                choiceGame = ReadTextInput("Qual e o melhor jogo do universo -> ");
+                if(choiceGame != bestGame)
+                {
+                    PrintLineAndWait("Resposta errada! Tua batata ta assando!");
+                    choiceGame = ReadTextInput("Qual e o melhor jogo do universo -> ");
+                    if(choiceGame != bestGame)
+                    {
+                        PrintLineAndWait("Voce nao sabe de nada mesmo... Vou procurar alguem que entende de jogos.");
+                    }
+                }
+            }
+            else if(choiceGame == bestGame)
+            {
+                Console.WriteLine("Voce acertou!!!! Voce entende de jogos!!");
+            }
+            Console.ReadKey();
+        }
+        static void PrintLineAndWait(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+        static string ReadTextInput(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
