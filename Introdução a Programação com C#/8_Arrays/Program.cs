40c8dbbd5f9fbdb339554b02ec9f21aa74e8e0b4
Allan <allanfelipedk@gmail.com>
2022-03-28 15:43:22 -0300

    8_Arrays

--- Introdução a Programação com C#/8_Arrays/Program.cs
+++ Introdução a Programação com C#/8_Arrays/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
 namespace _8_Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[]
+            {
+                -22, 2, 1
+            };
+
+            Console.WriteLine(numbers[1]);
+
+            //bounds check
+            int i = 5;
+            Console.WriteLine($"Tamanho do Array -> {numbers.Length}");
+            if(i >= 0 && i < numbers.Length)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            else
+            {
+                Console.WriteLine("indice fora do range do array!");
+            }
+            Console.ReadKey();
         }
     }
 }
