a3b7d30f40d45f5b70a676f256f560759be54941
Allan <allanfelipedk@gmail.com>
2022-04-30 17:15:30 -0300

    12_Classes_Dados_E_Funcionalidades

---
+++ Introdução a Programação com C#/12_Classes_Dados_E_Funcionalidades/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_Classes_Dados_E_Funcionalidades
+{
+    class Player
+    {
+        public string name = "Allan";
+        public int level = 1;
+
+        public void Teste()
+        {
+            Console.WriteLine($"Name -> {name}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)   
+        {
+            Player player = new Player();
+            Console.WriteLine($"O nome do player e {player.name} e o level- > {player.level}");
+            player.level = 100;
+            Console.WriteLine($"O level do player agora e -> {player.level}");
+            Console.ReadKey();
+        }
+    }
+}
