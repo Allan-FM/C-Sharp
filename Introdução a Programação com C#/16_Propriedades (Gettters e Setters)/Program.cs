13c363f6a5edb4574c24d982cd86acae7a6dd0c5
Allan <allanfelipedk@gmail.com>
2022-05-02 15:32:14 -0300

    16_Propriedades (Gettters e Setters)

---
+++ Introdução a Programação com C#/16_Propriedades (Gettters e Setters)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _16_Propriedades__Gettters_e_Setters_
+{
+    //public, private, protected, internal
+    // 1 - private por default
+    // 2 - usar getters e/ou setters para expor acesso aos membros da classe
+    // 3 - ter poucos metodos pulicos
+    class Player
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        private int level;
+        public int Level => level;
+        
+        //auto Property
+        public int Money { get; private set; }
+        public Player(string name, int level, int money)
+        {
+            this.name = name;
+            this.level = level;
+            Money = money;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("P1", 1, 100);
+            Console.WriteLine($"Name -> {player.Name}, {player.Level}, {player.Money}");
+            player.Name = "New Name";
+            Console.WriteLine($"Name -> {player.Name}");
+            Console.ReadKey();
+        }
+    }
+}
