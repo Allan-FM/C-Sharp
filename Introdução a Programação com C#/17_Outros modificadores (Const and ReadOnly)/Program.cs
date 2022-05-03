0bb4aa3097f53cadf488e88cb9449a6950c03db0
Allan <allanfelipedk@gmail.com>
2022-05-03 10:48:29 -0300

    17_Outros modificadores (Const and ReadOnly)

---
+++ Introdução a Programação com C#/17_Outros modificadores (Const and ReadOnly)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _17_Outros_modificadores__Const_and_ReadOnly_
+{
+    //Const e ReadOnly
+    // Readonly somente leitura e em membros (variaveis)
+    // Const mesma coisa porem temque inicializar a variavel
+    class Item
+    {
+        public readonly string Name;
+        public readonly int Price;
+
+        public const int minItemPrice = 2;
+
+        public Item(int price, string name)
+        {
+            if(price > minItemPrice)
+            {
+                Price = price;
+            }
+            else
+            {
+                Price = minItemPrice;
+            }
+            Name = name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
