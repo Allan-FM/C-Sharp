2a7a768bb53c3f2bf5839a4360623783abccb65a
Allan <allanfelipedk@gmail.com>
2022-06-16 18:24:46 -0300

    Curso-de-c-sharp - 1_Variaveis

---
+++ Curso-de-c-sharp/1_Variáveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            char letras = 'c';
+            float valor = 4.3f;
+
+            string nome = "Allan";
+
+            var auxiliar = 21;
+
+            Console.WriteLine($"{num} - {letras} - {valor} - {nome} - {auxiliar}");
+
+            Console.ReadKey();
+        }
+    }
+}
