f19ede64030af3e7154acaf7c2f14fae7162d0c4
Allan <allanfelipedk@gmail.com>
2022-06-17 12:32:01 -0300

    2_Escopo de variáveis

---
+++ Curso-de-c-sharp/2_Escopo de variáveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2_Escopo_de_variáveis
+{
+    class Program
+    {
+        //Variaval na classe
+        static int num = 10;
+        static void Main(string[] args)
+        {
+            int num2 = 0;
+            Teste();
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+        static void Teste()
+        {
+            //Console.WriteLine(num); -> nao pode ser usada
+            //Criar outra variavel  
+            int num3 = 0;
+            Console.WriteLine(num);//-> esta no escopo global
+        }
+    }
+}
