169fe4f293c02622e5c42452a78591b41de85361
Allan <allanfelipedk@gmail.com>
2022-06-18 17:44:05 -0300

    organizing the folders

---
+++ Linguagem C#/1_TiposDeVariaveis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1_TiposDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 1;
+            //myInt = "All";
+
+            var myVar = 2;
+
+            float myFloat = 2.5f;
+            
+            double myDouble = 321.3212d;
+
+            string myString = "Allala";
+
+            bool myBool = true;
+            
+            Console.WriteLine(myVar);
+            Console.WriteLine(myInt);
+            Console.WriteLine(myFloat);
+            Console.WriteLine(myString);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myDouble);
+
+            //Cast
+            float number1 = 4.4f;
+            float number2 = 5.2f;
+            int soma = (int)number1 + (int) number2;
+            Console.WriteLine(soma);
+
+            Console.ReadKey();
+        }
+    }
+}
