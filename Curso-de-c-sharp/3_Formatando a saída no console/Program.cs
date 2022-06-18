05cdbba04acc4082b9362b1f898353779d503e48
Allan <allanfelipedk@gmail.com>
2022-06-18 12:38:09 -0300

    3_Formatando a saída no console

---
+++ Curso-de-c-sharp/3_Formatando a saída no console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_Formatando_a_saída_no_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double valorDeCompra = 5;
+            double valorDaVenda;
+            double lucro = 0.1;
+            string nomeDoProduto = "Pastel";   
+
+            valorDaVenda = valorDeCompra + (valorDeCompra * lucro);
+
+            Console.WriteLine($"Nome do Produto -> {nomeDoProduto}");
+            Console.WriteLine($"Valor do Produto -> {valorDeCompra:c}");
+            Console.WriteLine($"Lucro do Produto -> {lucro}");
+            Console.WriteLine($"Valor de venda -> {valorDaVenda:c}");
+
+            Console.ReadKey();
+        }
+    }
+}
