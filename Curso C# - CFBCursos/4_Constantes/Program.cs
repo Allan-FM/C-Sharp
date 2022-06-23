d5ddeda5766d2b9bc15bbc68b4dcf77060a8c890
Allan <allanfelipedk@gmail.com>
2022-06-23 13:51:17 -0300

    4_Constantes

---
+++ Curso C# - CFBCursos/4_Constantes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_Constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Allan";
+            const double pi = 3.1415;
+            Console.WriteLine($"Nome: {name}");
+            Console.WriteLine($"Pi: {pi}");
+            Console.ReadKey();
+        }
+    }
+}
