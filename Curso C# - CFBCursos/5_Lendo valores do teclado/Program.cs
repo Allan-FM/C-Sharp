ea2d1d247da580885e2fcd32cba4ae7a12303a20
Allan <allanfelipedk@gmail.com>
2022-06-25 17:21:04 -0300

    5_Lendo valores do teclado

---
+++ Curso C# - CFBCursos/5_Lendo valores do teclado/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace _5_Lendo_valores_do_teclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int res;
+            string nome;
+
+            Console.Write("Digite o primeiro valor -> ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor -> ");
+            number2 = int.Parse(Console.ReadLine());
+            res = number1 + number2;
+            Console.WriteLine($"A soma de {number1} + {number2} = {res}");
+            /*
+            Console.Write("Digite o primeiro numero -> ");
+            nome = Console.ReadLine();
+            Console.WriteLine(nome);
+            */
+            Console.ReadKey();
+
+        }
+    }
+}
