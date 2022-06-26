d7709044379c604da37c5209c77dcc7f252d7bc0
Allan <allanfelipedk@gmail.com>
2022-06-26 19:04:41 -0300

    7_Enumeradores (enum)

---
+++ Curso C# - CFBCursos/7_Enumeradores (enum)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7_Enumeradores__enum_
+{
+    class Program
+    {
+        enum DaysOfTheWeek
+        {
+            Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado
+        }
+        static void Main(string[] args)
+        {
+            DaysOfTheWeek domingo = DaysOfTheWeek.Domingo;
+            Console.WriteLine(domingo);
+
+            DaysOfTheWeek segunda = (DaysOfTheWeek)1;
+            Console.WriteLine(segunda);
+
+            int terca  = (int)DaysOfTheWeek.Terca;
+            Console.WriteLine(terca);
+
+            Console.ReadKey();
+        }
+    }
+}
