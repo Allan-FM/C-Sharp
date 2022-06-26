5c77cab2c2b74fe8660958c3da6aa12f8ba39b37
Allan <allanfelipedk@gmail.com>
2022-06-26 18:53:58 -0300

    6_Operações de Bitwise

---
+++ Curso C# - CFBCursos/6_Operações de Bitwise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6_Operações_de_Bitwise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10; 
+            //num = num << 1;
+
+            //Console.WriteLine(num); -> 20
+
+            num = num >> 1;
+            Console.WriteLine(num); // -> 5 
+
+            Console.ReadKey();
+        }
+    }
+}
