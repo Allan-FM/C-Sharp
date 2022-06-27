d5f043d144502e999754dbe2a87a8e256093a2a1
Allan <allanfelipedk@gmail.com>
2022-06-27 15:22:32 -0300

    8_Conversões de tipos (typecast)

---
+++ Curso C# - CFBCursos/8_Conversões de tipos (typecast)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _8_Conversões_de_tipos__typecast_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            short shortnumber= (short)number;
+            float number2 = 2.3f;
+            int res = (int)number2;
+            Console.WriteLine(res);
+            Console.WriteLine(shortnumber);
+            Console.ReadKey();
+        }
+    }
+}
