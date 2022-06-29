631baef7eab4a57d251ecd426b99ea411ed742c4
Allan <allanfelipedk@gmail.com>
2022-06-29 14:27:53 -0300

    9_Comando condicional IF

---
+++ Curso C# - CFBCursos/9_Comando condicional IF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _9_Comando_condicional_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota = 0;
+            Console.Write("Digite a sua media final -> ");
+            nota = int.Parse(Console.ReadLine());
+            if(nota >= 60)
+            {
+                Console.WriteLine($"Aprovado! -> Nota: {nota}");
+            }
+            else
+            {
+                Console.WriteLine($"Reprovado! -> Nota: {nota}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
