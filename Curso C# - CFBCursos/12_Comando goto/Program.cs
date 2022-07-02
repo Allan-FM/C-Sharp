27a4249f44f418a64ac3b6113fbce7509dbee38b
Allan <allanfelipedk@gmail.com>
2022-07-02 13:34:24 -0300

    12_Comando goto

---
+++ Curso C# - CFBCursos/12_Comando goto/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _12_Comando_goto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo = 0;
+            char escolha;
+
+            inicio:
+            Console.Clear();
+
+            Console.WriteLine("Belo Horizonte/MG a Vitoria/ES");
+            Console.WriteLine("Escolha um meio de transporte -> [a]Aviao | [c]Carro | [o] Onibus");
+
+            escolha = char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 'a':
+                    tempo = 50;
+                    Console.WriteLine($"O tempo da viagem vai ser de {tempo} minutos");
+                    break;
+                case 'c':
+                    tempo = 480;
+                    Console.WriteLine($"O tempo da viagem vai ser de {tempo} minutos (8 horas)");
+                    break;
+                case 'o':
+                    tempo = 660;
+                    Console.WriteLine($"O tempo da viagem vai ser de {tempo} minutos (11 horas)");
+                    break;
+                default:
+                    Console.WriteLine("Digite um opcao valida ");
+                    break;
+            }
+            Console.Write("Calcular novamente? [s/n] -> ");
+            escolha = char.Parse(Console.ReadLine());
+            if(escolha == 's' || escolha == 'S')
+            {
+                goto inicio;//
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Fim do programa!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
