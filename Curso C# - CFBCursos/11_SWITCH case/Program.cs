1acecce0f6f2e49a09a7822c20cbd383446e4d10
Allan <allanfelipedk@gmail.com>
2022-07-01 21:25:03 -0300

    11_SWITCH case

---
+++ Curso C# - CFBCursos/11_SWITCH case/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _11_SWITCH_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo = 0;
+            char escolha;
+            Console.WriteLine("Belo Horizonte/MG a Vitoria/ES");
+            Console.WriteLine("Escolha um meio de transporte -> [a]Aviao | [c]Carro | [o] Onibus");
+
+            escolha = char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 'a':
+                    tempo = 50;
+                    Console.WriteLine($"O tempo da viagem vai ser de {tempo} minutos");
+                    break;
+                case 'c':
+                    tempo = 480;
+                    Console.WriteLine($"O tempo da viagem vai ser de {tempo} minutos (8 horas)");
+                    break;
+                case 'o':
+                    tempo = 660;
+                    Console.WriteLine($"O tempo da viagem vai ser de {tempo} minutos (11 horas)");
+                    break;
+                default:
+                    Console.WriteLine("Digite um opcao valida ");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
