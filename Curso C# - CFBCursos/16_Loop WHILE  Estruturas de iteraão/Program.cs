dd3c67939ad09def52e8b5c2e79e6ceac301b4a1
Allan <allanfelipedk@gmail.com>
2022-07-05 13:45:00 -0300

    16_Loop WHILE  Estruturas de iteraão

---
+++ Curso C# - CFBCursos/16_Loop WHILE  Estruturas de iteraão/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _16_Loop_WHILE__Estruturas_de_iteraão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i = 0;
+            while (i < arr.Length)
+            {
+                Console.WriteLine(arr[i]);
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
