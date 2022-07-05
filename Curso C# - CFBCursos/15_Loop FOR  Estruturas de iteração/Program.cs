0eb22b28235ce7c8645df40ab82512fb40684400
Allan <allanfelipedk@gmail.com>
2022-07-05 13:12:13 -0300

    15_Loop FOR  Estruturas de iteração

---
+++ Curso C# - CFBCursos/15_Loop FOR  Estruturas de iteração/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _15_Loop_FOR__Estruturas_de_iteração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int[] arr = new int[] { 10, 2, 4, 5, 6, 7, 8 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
