20a50d5f26d46a5b7bebaf48a720b3ade1331de9
Allan <allanfelipedk@gmail.com>
2022-07-06 13:06:18 -0300

    17_Loop DO WHILE  Estruturas de iteração

---
+++ Curso C# - CFBCursos/17_Loop DO WHILE  Estruturas de iteração/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _17_Loop_DO_WHILE__Estruturas_de_iteração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha ="123";
+            string senhaDoUsuario;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha: ");
+                senhaDoUsuario = Console.ReadLine();
+
+            } while (senha != senhaDoUsuario);
+
+            Console.WriteLine("Senha correta!");
+            Console.ReadKey();
+        }
+    }
+}
