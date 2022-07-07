1a763715a1291828491aa8e59d0adde9f8669527
Allan <allanfelipedk@gmail.com>
2022-07-07 13:29:19 -0300

    18_Loop FOREACH  Estruturas de iteração

---
+++ Curso C# - CFBCursos/18_Loop FOREACH  Estruturas de iteração/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _18_Loop_FOREACH__Estruturas_de_iteração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {11, 23, 45};
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
