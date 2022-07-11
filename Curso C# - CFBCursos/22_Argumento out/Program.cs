840836546a6c2d6baf2c35d84a9ed91040aa6456
Allan <allanfelipedk@gmail.com>
2022-07-11 19:04:53 -0300

    22_Argumento out

---
+++ Curso C# - CFBCursos/22_Argumento out/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Cache;
+
+namespace _22_Argumento_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividendoMain = 10;
+            int divisorMain = 5;
+            int rest;
+            int quoce = Dividadendo(dividendoMain, divisorMain,out rest);
+            Console.WriteLine($"quociente = {quoce} \nresto = {rest}");
+            Console.ReadKey();
+        }
+        static int Dividadendo(int dividendo, int divisor, out int resto)
+        {
+            int cosiente;
+            cosiente = dividendo / divisor;
+            resto = dividendo % divisor;
+            return cosiente;
+        }
+    }
+}
