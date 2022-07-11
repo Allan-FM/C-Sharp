62c1d97c479b677849aec2cc53ebf59ea5549531
Allan <allanfelipedk@gmail.com>
2022-07-10 23:12:03 -0300

    21_Passagem por valor e por referência (ref)

---
+++ Curso C# - CFBCursos/21_Passagem por valor e por referência (ref)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _21_Passagem_por_valor_e_por_referência
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Dobrar(ref num);
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+        static void Dobrar(ref int valor)   
+        {
+            valor *= 2;
+        }
+    }
+}
