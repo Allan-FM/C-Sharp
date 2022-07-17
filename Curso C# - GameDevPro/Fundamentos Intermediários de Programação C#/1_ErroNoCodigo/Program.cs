30499fc88133ea0411ce351e80badb3088acc191
Allan <allanfelipedk@gmail.com>
2022-07-17 14:53:09 -0300

    I understand how memory works with the difference between struct and class

---
+++ Curso C# - GameDevPro/Fundamentos Intermediários de Programação C#/1_ErroNoCodigo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _1_ErroNoCodigo
+{
+    struct Player
+    {
+        public int Coins;
+        public void ExpendCoins()
+        {
+            Coins--;
+        }
+    }
+    struct UI
+    {
+        public Player Player;
+        public void ShowCoins()
+        {
+            Console.WriteLine($"Voce tem {Player.Coins} coins");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player
+            {
+                Coins = 10
+            };
+            UI ui = new UI
+            {
+                Player = player
+            };
+            ui.ShowCoins();
+            Console.WriteLine();
+            while (player.Coins > 0)
+            {
+                Console.WriteLine("Aperte qualquer tecla para gastar os coins!");
+                Console.ReadKey();
+                Console.WriteLine();
+                player.ExpendCoins();
+                ui.ShowCoins();
+            }
+            Console.WriteLine("\n\n-------FIM DO PROGRAMA-------------");
+            Console.ReadKey();
+        }
+    }
+}
