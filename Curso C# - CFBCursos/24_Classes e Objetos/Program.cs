215f4d7631c2d326741dac42410d29767e1e91e4
Allan <allanfelipedk@gmail.com>
2022-07-20 21:33:56 -0300

    24_Classes e Objetos

---
+++ Curso C# - CFBCursos/24_Classes e Objetos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _24_Classes_e_Objetos
+{
+    class Player
+    {
+        public int energia = 100;
+        public bool vida = true;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Console.WriteLine($"Energia do player {player.energia}");
+            Console.ReadKey();
+        }
+    }
+}
